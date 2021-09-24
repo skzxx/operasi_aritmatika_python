8ced87786556c9ec79260b87bda6f580ee2d37ce
skzxx <rrachmawan1965@gmail.com>
2021-09-24 23:11:00 +0700

    add

---
+++ FUCKING_PROGRAM.py
@@ -0,0 +1,46 @@
+#Operasi aritmatika
+
+#Penjumlahan
+
+Operator_Aritmatika_A = 10;
+Operator_Aritmatika_B = 3;
+Operator_Hasil_Kalkulasi = (Operator_Aritmatika_A + Operator_Aritmatika_B);
+print("1.Penjumlahan dari ", Operator_Aritmatika_A, "+", Operator_Aritmatika_B, "=", Operator_Hasil_Kalkulasi);
+
+#Pengurangan
+
+Operator_Hasil_Kalkulasi = (Operator_Aritmatika_A - Operator_Aritmatika_B);
+print("2.Jumlah dari ", Operator_Aritmatika_A, "-", Operator_Aritmatika_B, "=", Operator_Hasil_Kalkulasi);
+
+#Perkalian
+
+Operator_Hasil_Kalkulasi = (Operator_Aritmatika_A * Operator_Aritmatika_B);
+print("3.Jumlah dari ", Operator_Aritmatika_A, "*", Operator_Aritmatika_B, "=", Operator_Hasil_Kalkulasi);
+
+#Pembagian
+
+Operator_Hasil_Kalkulasi = (Operator_Aritmatika_A / Operator_Aritmatika_B);
+print("2.Jumlah dari ", Operator_Aritmatika_A, "/", Operator_Aritmatika_B, "=", Operator_Hasil_Kalkulasi);
+
+#eksponen
+
+Operator_Hasil_Kalkulasi = (Operator_Aritmatika_A ** Operator_Aritmatika_B);
+print("4.Jumlah dari ", Operator_Aritmatika_A, "**", Operator_Aritmatika_B, "=", Operator_Hasil_Kalkulasi);
+
+#Modulus
+
+Operator_Hasil_Kalkulasi = (Operator_Aritmatika_A % Operator_Aritmatika_B);
+print("5.Jumlah dari ", Operator_Aritmatika_A, "%", Operator_Aritmatika_B, "=", Operator_Hasil_Kalkulasi);
+
+#Floor division
+Operator_Aritmatika_Floor_Division_A = 3.48;
+Operator_Aritmatika_Floor_Division_B = 7.827;
+Operator_Hasil_Kalkulasi_Floor_Dision = (Operator_Aritmatika_Floor_Division_A // Operator_Aritmatika_Floor_Division_B);
+print("6.Division operator dari ", Operator_Aritmatika_Floor_Division_A, "//", Operator_Aritmatika_Floor_Division_B, "=", Operator_Hasil_Kalkulasi_Floor_Dision);
+print("\\\\Prioritas Operasi\\\\");
+Operator_Aritmatika_Oxcc12 = 3;
+Operator_Aritmatika_Oydd12 = 2;
+Operator_Aritmatika_Ozee12 = 4;
+OPERATOR_HASIL_KALKULASI_PRIORITAS = Operator_Aritmatika_Oxcc12 ** Operator_Aritmatika_Oydd12 * Operator_Aritmatika_Ozee12 + Operator_Aritmatika_Oxcc12 / Operator_Aritmatika_Oydd12 - Operator_Aritmatika_Oxcc12 % Operator_Aritmatika_Ozee12 // Operator_Aritmatika_Oxcc12;
+print("Hasil dari kalkulasi ", Operator_Aritmatika_Oxcc12, "**", Operator_Aritmatika_Oydd12, "*", Operator_Aritmatika_Ozee12, "+", Operator_Aritmatika_Oxcc12, "/", Operator_Aritmatika_Oydd12, "-", Operator_Aritmatika_Oxcc12, "%", Operator_Aritmatika_Ozee12, "//", Operator_Aritmatika_Oxcc12, "=", OPERATOR_HASIL_KALKULASI_PRIORITAS );
+print("-Hasil kalkulasi diatas bosa begotu karena ada operator yang diprioritaskan yaitu 1. (), 2. Exponensial, 3.Perkalian, 4.Pertambahan.");
